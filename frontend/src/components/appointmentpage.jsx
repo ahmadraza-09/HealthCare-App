4bf403091ddb3e208ddff3771990bd38cfe8f3a7
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-04-24 15:04:30 +0530

    Appointment API Connection

--- frontend/src/components/appointmentpage.jsx
+++ frontend/src/components/appointmentpage.jsx
@@ -1,7 +1,91 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import '../css/appointmentpage.css'
 
 const AppointmentPage = () => {
+
+    const [name, getName] = useState('');
+    const [dateofbirth, getDateofbirth] = useState('');
+    const [gender, getGender] = useState('');
+    const [concern, getConcern] = useState('');
+    const [mobilenumber, getMobilenumber] = useState('');
+    const [formerror, getFormerror] = useState('');
+    const nameregex = /^[a-z A-Z]{2,15}$/;
+    const mobilenumberregex = /^[0-9]{10}$/;
+
+    const nameHandler = (event) => {
+        getName(event.target.value);
+    }
+
+    const dateofbirthHandler = (event) => {
+        getDateofbirth(event.target.value);
+    }
+
+    const genderHandler = (event) => {
+        getGender(event.target.value);
+    }
+
+    const concernHandler = (event) => {
+        getConcern(event.target.value);
+    }
+
+    const mobilenumberHandler = (event) => {
+        getMobilenumber(event.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault(); {
+            if (name === '') {
+                getFormerror("Enter your Name");
+                return false;
+            } else if (!name.match(nameregex)) {
+                getFormerror("Enter Name in Letters");
+                return false;
+            } else if (dateofbirth === '') {
+                getFormerror("Select your Date of Birth");
+                return false;
+            } else if (gender === '') {
+                getFormerror("Select your Gender");
+                return false;
+            } else if (concern === '') {
+                getFormerror("Select Your Health Concern");
+                return false;
+            } else if (mobilenumber === '') {
+                getFormerror("Enter your Mobile Number");
+                return false;
+            } else if (!mobilenumber.match(mobilenumberregex)) {
+                getFormerror("Invalid Number! Please write only numbers");
+                return false;
+            } else {
+                    const appointmentData = { name, dateofbirth, gender, concern, mobilenumber, };
+                    axios.post('http://localhost:3050/auth/appointment', appointmentData)
+                        .then((response) => {
+                        const responseData = response.data;
+                        console.log("Response data:", responseData);
+                            const message = responseData.message;
+                            console.log("Message:", message);
+                            if (message === "All fields are required") {
+                                getFormerror("All fields are required");
+                            } else if (response.data.message === "Appointment limit reached for today. Try again tomorrow.") {
+                                getFormerror("Appointment limit reached for today. Try again tomorrow.");
+                            } else {
+                                alert("Appointment Booked Successfully");
+                                getName('');
+                                getDateofbirth('');
+                                getGender('');
+                                getConcern('');
+                                getMobilenumber('');
+                                getFormerror('');
+                            }
+                    })
+                    .catch((error) => {
+                        console.error("Error contacting:", error);
+                        getFormerror("appointment Request failed");
+                    });
+                }
+          }
+    }
+
   return (
     <>
       <div className="appointment-section">
@@ -11,21 +95,22 @@ const AppointmentPage = () => {
 
         <div className="appointment-section-right">
             <h2>Book Your Appointment</h2>
-            <form>
+            <form onSubmit={submitHandler}>
+                {formerror && <p className="error-message">{formerror}</p>}
                 <div className='line-one'>
                     <div className='flex'>
                         <label>Name</label>
-                        <input type="text" placeholder='Name'/>
+                        <input type="text" placeholder="Name" value={name} onChange={nameHandler} />
                     </div>
                     <div className='flex'>
                         <label>Date Of Birth</label>
-                        <input type="date"/>
+                        <input type="date" value={dateofbirth} onChange={dateofbirthHandler} />
                     </div>
                 </div>
 
                 <div className='flex'>
                     <label>Gender</label>
-                    <select id="gender" name="gender">
+                    <select id="gender" value={gender} onChange={genderHandler}>
                         <option value="">Select Gender</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
@@ -35,7 +120,7 @@ const AppointmentPage = () => {
 
                 <div className='flex'>
                     <label>Select Your Health Concern</label>
-                    <select id="gender" name="gender">
+                    <select id="gender" name="gender" value={concern} onChange={concernHandler}>
                         <option value="">Select</option>
                         <option value="Headache">Headache</option>
                         <option value="Fever">Fever</option>
@@ -47,14 +132,10 @@ const AppointmentPage = () => {
 
                 <div className='flex'>
                     <label>Mobile Number</label>
-                    <input type="text" placeholder='Mobile Number'/>
+                    <input type="text" placeholder="Enter Mobile Number" value={mobilenumber} onChange={mobilenumberHandler} />
                 </div>
 
-                <div className='flex'>
-
-                </div>
-
-                <button>Book Now</button>
+                <button type='submit'>Book Now</button>
             </form>
         </div>
       </div>
--- frontend/src/components/contactcomp.jsx
+++ frontend/src/components/contactcomp.jsx
@@ -1,30 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import '../css/contact.css'
 
 const ContactComp = () => {
+
+    const [name, getName] = useState('');
+    const [mobilenumber, getMobilenumber] = useState('');
+    const [email, getEmail] = useState('');
+    const [message, getMessage] = useState('');
+    const [formerror, getFormerror] = useState('');
+    const nameregex = /^[a-z A-Z]{2,15}$/;
+    const mobilenumberregex = /^[0-9]{10}$/;
+    const emailregex = /^[a-z A-Z 0-9]+@[a-z]+\.[a-z]{2,6}$/; 
+
+    const nameHandler = (event) => {
+        getName(event.target.value);
+    }
+
+    const emailHandler = (event) => {
+        getEmail(event.target.value);
+    }
+
+    const mobilenumberHandler = (event) => {
+        getMobilenumber(event.target.value);
+    }
+
+    const messageHandler = (event) => {
+        getMessage(event.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault(); {
+            if (name === '') {
+                getFormerror("Enter your Name");
+                return false;
+            } else if (!name.match(nameregex)) {
+                getFormerror("Enter Name in Letters");
+                return false;
+            } else if (email === '') {
+                getFormerror("Enter your Email");
+                return false;
+            } else if (!email.match(emailregex)) {
+                getFormerror("Enter Valid Email");
+                return false;
+            } else if (mobilenumber === '') {
+                getFormerror("Enter your Mobile Number");
+                return false;
+            } else if (!mobilenumber.match(mobilenumberregex)) {
+                getFormerror("Invalid Number! Please write only numbers");
+                return false;
+            } else {
+                    const contactData = { name, mobilenumber, email, message };
+                    axios.post('http://localhost:3050/auth/contact', contactData)
+                        .then((response) => {
+                        const responseData = response.data;
+                        console.log("Response data:", responseData);
+                            const message = responseData.message;
+                            console.log("Message:", message);
+                            if (message === "Please fill the form") {
+                                getFormerror("Please fill the form");
+                            } else if (message === 'You have already submitted a query today. Please try again tomorrow.') {
+                                getFormerror("You have already submitted a query today. Please try again tomorrow.");
+                            } else {
+                                alert("Message Sent Successfully");
+                                getName('');
+                                getMobilenumber('');
+                                getEmail('');
+                                getMessage('');
+                                getFormerror('');
+                            }
+                    })
+                    .catch((error) => {
+                        console.error("Error contacting:", error);
+                        getFormerror("Contact Request failed");
+                    });
+                }
+          }
+    }
+
   return (
     <>
       <div className="contat-page">
         <div className="contat-form">
           <img src="images/contact.png" alt="" />
 
-          <form>
+          <form onSubmit={submitHandler}>
             <h2>Contact Us</h2> 
+            {formerror && <p className="error-message">{formerror}</p>}
             <div className="input-box">
               <label>Name</label>
-              <input type="text" placeholder='Enter your name'/>
+              <input type="text" placeholder='Enter your name' value={name} onChange={nameHandler} />
             </div>
             <div className="input-box">
               <label>Email</label>
-              <input type="text" placeholder='Enter your email'/>
+              <input type="email" placeholder='Enter your email' value={email} onChange={emailHandler} />
             </div>
             <div className="input-box">
               <label>Mobile Number</label>
-              <input type="text" placeholder='Enter your mobile number'/>
+              <input type="text" placeholder='Enter your mobile number' value={mobilenumber} onChange={mobilenumberHandler} />
             </div>
             <div className="input-box">
               <label>Message</label>
-              <textarea type="text" placeholder='Enter your query here...'/>
+              <textarea typeof='text' placeholder='Enter your query here...' value={message} onChange={messageHandler} />
             </div>
             <button type='submit'>Send</button>
           </form>
