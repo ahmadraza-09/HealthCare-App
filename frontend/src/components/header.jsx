8a22f6b0b0cac64b9e46ebfb9e9a6ce9f705cd4f
Ahmad Raza <ahmadraza20082003@gmail.com>
2025-07-25 15:14:49 +0530

    Added Tailwind

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,53 +1,119 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-
   const navigate = useNavigate();
 
-  const role = localStorage.getItem('role');
-  const isLoggedIn = localStorage.getItem('token') !== null;
-  const id = localStorage.getItem('id');
+  const [showMenu, setShowMenu] = useState(false);
+
+  const role = localStorage.getItem("role");
+  const isLoggedIn = localStorage.getItem("token") !== null;
+  const id = localStorage.getItem("id");
+
+  const showMenuFunc = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <>
-      <nav>
-        <input type="checkbox" id='check'/>
-
-        <div className="logo">
-            <label htmlFor="check"><i class="fa-solid fa-bars"></i></label>
-            <h2 onClick={() => {navigate('/')}}>Health<span>Care</span></h2>
+      <nav className="w-full h-[60px] bg-[#135D66] flex px-5 items-center justify-between text-[#E3FEF7] fixed z-[999]">
+        <div className="logo text-[#FFF7FC] text-2xl font-semibold flex items-center justify-start gap-5">
+          <label htmlFor="check" onClick={showMenuFunc} className="sm:hidden">
+            <i class="fa-solid fa-bars "></i>
+          </label>
+          <h2
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Health<span className="text-yellow-500">Care</span>
+          </h2>
         </div>
 
-        <ul className="menu">
-            <label htmlFor="check"><i class="fa-solid fa-close"></i></label>
-            <li onClick={() => {navigate('/')}}>Home</li>
-            <li onClick={() => {navigate('/doctor')}}>Doctor</li>
-            <li onClick={() => {navigate('/about')}}>About</li>
-            <li onClick={() => {navigate('/contact')}}>Contact</li>
-            {isLoggedIn &&
-            <li onClick={() => {navigate('/appointment')}}>Book Appointment</li>
-            }
+        <ul
+          className={`menu w-full h-screen sm:static sm:h-0 sm:w-fit sm:flex-row items-center justify-start gap-8 sm:text-lg font-normal sm:flex absolute top-[60px] py-5 px-5 left-0 bg-[#135D66] text-xl flex flex-col ${
+            showMenu === true ? "flex" : "hidden"
+          }`}
+        >
+          <li
+            onClick={() => {
+              navigate("/");
+            }}
+            className="cursor-pointer"
+          >
+            Home
+          </li>
+          <li
+            onClick={() => {
+              navigate("/doctor");
+            }}
+            className="cursor-pointer"
+          >
+            Doctor
+          </li>
+          <li
+            onClick={() => {
+              navigate("/about");
+            }}
+            className="cursor-pointer"
+          >
+            About
+          </li>
+          <li
+            onClick={() => {
+              navigate("/contact");
+            }}
+            className="cursor-pointer"
+          >
+            Contact
+          </li>
+          {isLoggedIn && (
+            <li
+              onClick={() => {
+                navigate("/appointment");
+              }}
+              className="cursor-pointer"
+            >
+              Book Appointment
+            </li>
+          )}
         </ul>
 
-        
         <div className="login">
-            {(isLoggedIn && role === 'patient') ? (
-              <div className="header-profile" onClick={() => {navigate(`/profile/${id}`)}}>
-                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
-              </div>
-            ):(isLoggedIn && role === 'doctor') ? (
-              <div className="admin-panel" onClick={() => {navigate(`/adminpanel`)}}>
-                <button>Admin Panel</button>
-              </div>
-            ):(
-              <button onClick={() => {navigate('/login')}}>Login</button>
-            )}
-            
+          {isLoggedIn && role === "patient" ? (
+            <div
+              className="header-profile"
+              onClick={() => {
+                navigate(`/profile/${id}`);
+              }}
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                alt=""
+              />
+            </div>
+          ) : isLoggedIn && role === "doctor" ? (
+            <div
+              className="admin-panel"
+              onClick={() => {
+                navigate(`/adminpanel`);
+              }}
+            >
+              <button>Admin Panel</button>
+            </div>
+          ) : (
+            <button
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </button>
+          )}
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Appointment from './appointment'
+import React from "react";
+import Appointment from "./appointment";
 
 const Hero = () => {
   return (
     <>
-      <div className="hero-section">
-        <img src="https://themewagon.github.io/orthoc/images/hero-bg.png" alt="" />
+      <div className="hero-section w-full h-fit sm:h-[500px] pt-[60px]">
+        <img
+          src="https://themewagon.github.io/orthoc/images/hero-bg.png"
+          alt=""
+          className="w-full h-full"
+        />
 
-        <div className="our-title">
-            <h2>We <span>Care</span> Your <span>Health</span></h2>
-        </div>
-
-        <div className="appointment-box">
-            <Appointment/>
+        <div className="our-title absolute sm:top-[150px] top-[100px] sm:left-[150px] left-[10px] sm:text-4xl text-xl text-[#b7ffeb] font-bold">
+          <h2>
+            We <span className="text-yellow-500">Care</span> Your{" "}
+            <span className="text-white">Health</span>
+          </h2>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
